0757c39e78053baee3aae1e742bbbda7fd25c7df
torreslucs23 <torreslucs23@gmail.com>
2023-12-19 21:28:31 -0300

    created attack funcionality

---
+++ app.js
@@ -0,0 +1,25 @@
+const getRandomvalue = (min, max) => {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+const app = Vue.createApp({
+  data() {
+    return {
+      playerHealth: 100,
+      monsterHealth: 100,
+    };
+  },
+  methods: {
+    attackMonster() {
+      const attackValue = getRandomvalue(5, 20);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
+    },
+    attackPlayer() {
+      const attackValue = getRandomvalue(5, 23);
+      this.playerHealth -= attackValue;
+    },
+  },
+});
+
+app.mount("#game");
